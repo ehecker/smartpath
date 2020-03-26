f959e7984728aab67dfb9497b414ffc6fde2435a
ehecker <ezhecker@gmail.com>
2020-03-25 23:11:19 -0700

    Adjust BFS

--- src/algorithms/polytreenode.js
+++ src/algorithms/polytreenode.js
@@ -6,9 +6,22 @@ export default class PolyTreeNode {
         this.children = [];
     }
 
+    bfs(target) {
+        let queue = [];
+
+        queue.push(this);
+
+        while (queue.length > 0) {
+            let currentNode = queue.shift();
+            if (currentNode.value === target) return currentNode;
+            queue.push(...currentNode.children);
+        }
+    }
+
     addParent(node) {
-        if (this.parent !== null && this.parent !== node) {
-            this.parent.children
+        if (this.parent !== null) {
+            let index = this.parent.children.indexOf(this); // Does indexOf work for finding objects? Need to research.
+            this.parent.children.splice(index, 1);
         }
 
         this.parent = node;
@@ -16,6 +29,15 @@ export default class PolyTreeNode {
         if (this.parent) {
             this.parent.children.push(this);
         }
+    }
+
+    addChild(childNode) {
+        this.children.push(childNode);
+    }
+
+    removeChild(childNode) {
+        let index = this.children.indexOf(childNode);
+        this.children.splice(index, 1); // Same as above here
     }
 
 }
